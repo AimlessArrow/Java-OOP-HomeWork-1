bf8f15da0d8aa92da88a3ffd4cc262adb0915973
Timur Beldenov <AimlessArrow@users.noreply.github.com>
2019-01-24 12:07:55 +0600

    Add files via upload

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass {
   public static void main(String args[]) {
-    /* Класс Cat */
+    /* Cat */
     Cat myCat = new Cat();
     myCat.setBreed("Aegean");
     myCat.setHairColour("White and grey");
@@ -12,12 +12,12 @@ public class MyClass {
     Cat myCheshireCat = new Cat("Munchkin", "White", 5, "Calm", "Snowflake");
     System.out.println(myCheshireCat);
     
-    /* Класс Triangle */
+    /* Triangle */
     Triangle myTriangle = new Triangle(2, 2, 2);
     System.out.println(myTriangle);
     System.out.println("Triangle area: " + myTriangle.calcArea());
     
-    /* Класс Vector3D */
+    /* Vector3D */
     Vector3D myVector = new Vector3D(1, 2, 3);
     Vector3D myAnotherVector = new Vector3D(3, 2, 1);
     Vector3D resultVector = new Vector3D(0, 0, 0);
@@ -28,7 +28,7 @@ public class MyClass {
     resultVector = myVector.calcVectorProduct(myAnotherVector);
     System.out.println(resultVector);
     
-    /* Классы Phone и Network */
+    /* Phone & Network */
     Phone myPhone = new Phone("+77771231234");
     Phone targetPhone = new Phone("+77773211234");
     Network tele2 = new Network("Tele2");
